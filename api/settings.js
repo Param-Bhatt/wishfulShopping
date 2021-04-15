dbe2388d25226815c17b616f1822811f52de6e50
param-19 <f20180862@hyderabad.bits-pilani.ac.in>
2021-04-15 22:51:28 +0530

    Update settings.js
    Removing private information

--- api/settings.js
+++ api/settings.js
@@ -4,22 +4,22 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 var serverSettings = {
+    //UPDATE DETAILS HERE AS REQUIRED
     PROJECT_DIR: __dirname,
     PROJECT_LIB: lib,
     PORT : process.env.PORT,
-    MAIL_USER : 'padb19122000@gmail.com',
-    MAIL_PASSWORD : 'paramforever19',
-    MAIL_OAUTH_CLIENT_ID : '493047262649-qbjgnecqikv8dpotldphijf1u0el1hhk.apps.googleusercontent.com',
-    MAIL_OAUTH_CLIENT_SECRET : 'JEF6225yBUepYwxZgbfk3y-R',
-    MAIL_OAUTH_REFRESH_TOKEN : '1//04f2avedAT99jCgYIARAAGAQSNwF-L9Ir-ofixisUWll6dEvBa8gh94oDuqWqR0MJmY0a4Uu13mrd4wS4azhjzJyaKLoA45WvhGg',
-    MAIL_OAUTH_ACCESS_TOKEN : 'ya29.a0AfH6SMAw7V3ftZqjsCw82DQkBhgreSgagFeUaT-FyzKy5-GpX9rZncuuNXjnrHZa-eXy1loDMNSkTNO_aLorl2WYnloHtmRpzXT-Te4F907t1z7L92ke1mIZHFQE_1FQFzWHs04gpHfiu2PM8jtgKmdetIXs',
-    DB_NAME : 'pabby',
-    DB_PASSWORD : 'paramforever19',
+    MAIL_USER : '<emailid@gmail.com>',
+    MAIL_PASSWORD : '<emailpassword>',
+    MAIL_OAUTH_CLIENT_ID : '<google oauth2 client id>',
+    MAIL_OAUTH_CLIENT_SECRET : '<google oauth2 client secret>',
+    MAIL_OAUTH_REFRESH_TOKEN : '<google oauth2 refresh token>',
+    MAIL_OAUTH_ACCESS_TOKEN : '<google oauth2 access token>',
+    DB_NAME : '<mongodb name>',
+    DB_PASSWORD : '<mongodb password>',
     DB_HOST: process.env.DB_HOST || 'localhost',
-    DB_USER: process.env.DB_USER || 'root',
-    DB_MYSQL_PASSWORD: process.env.DB_PASSWORD || 'Param@2611',
+    DB_USER: process.env.DB_USER || '<mysql user>',
+    DB_MYSQL_PASSWORD: process.env.DB_PASSWORD || '<mysql password>',
     DB_CONNECTION_LIMIT: 10,
-    JWT_PRIVATE_KEY: process.env.JWT_PRIVATE_KEY || 'ItsaNamaZoncLONE!!!!!',
-    CRYPTO_KEY: process.env.CRYPTO_KEY,
+    
 }
 module.exports = serverSettings
