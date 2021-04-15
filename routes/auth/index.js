1e3ef1b342e1ade8e4e0591db66e3cd488dd3fac
Param-Bhatt <padb19122000@gmail.com>
2021-04-15 22:48:40 +0530

    added database schemas

--- routes/auth/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-
-//router.use('/', require('./auth.js'))
-router.use('/signup/', require('./signup.js'))
-router.use('/login/', require('./login.js'))
-router.use('/register', require('./register.js'))
-
-module.exports = router
