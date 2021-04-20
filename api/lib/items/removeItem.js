6adfae91bf372fc345422fb920a3583abc5c7271
Param-Bhatt <padb19122000@gmail.com>
2021-04-20 19:23:53 +0530

    Minor change to remove items

--- api/lib/items/removeItem.js
+++ api/lib/items/removeItem.js
@@ -14,8 +14,8 @@ var removeItem = (name, price) => {
         users_database.call(sql, params)
         .then((res) => {
             if(res.userType != 'shopper'){
-                sql = 'DELETE from items where name = ? AND price = ?'
-                params = [name, price]
+                sql = 'DELETE from items where name = ? AND price = ? and sellerEmail = ?'
+                params = [name, price, res.userType]
                 database.call(sql, params)
                 .then((result) => {
                     console.log(result)
